56b9a5d31750ff9abf4b559b779af28195f2b63a
marijadragicevic <majadragicevic.111@gmail.com>
2021-11-29 19:11:43 +0100

    1.commit/ JS/ IF statement

---
+++ domaci_zadaci 05/D_5_3303_Marija_Dragicevic/script.js
@@ -0,0 +1,30 @@
+// 1.zadatak
+let p = 1500;
+let m = 2000;
+let cena = 1200;
+console.log(`Pera je dobio kusur u iznosu od ${p - cena} dinara.`);
+console.log(`Mika je dobio kusur u iznosu od ${m - cena} dinara.`);
+
+// 2.zadatak
+let n = 48;
+let a = 12;
+let drugiDan = a + 2;
+let doKraja = n - a - drugiDan;
+console.log(`Do kraja knjige citaocu je ostalo ${doKraja} poglavlja.`);
+
+if (doKraja < n / 2) {
+    console.log(`Ostalo je da se procita jos manje od polovine knjige.`);
+}
+else {
+    console.log(`Ostalo je da se procita vise od polovine knjige.`);
+}
+
+// 3.zadatak
+let u = 5000; // 1kcal = 4.1868 kJ
+let kcal = u / 4.1868
+if (kcal < 2000) {
+    console.log(`Potrebno je povecati dnevni unos kcal.`);
+}
+else {
+    console.log(`Nije potrebno povecati dnevni unos kcal.`);
+}
